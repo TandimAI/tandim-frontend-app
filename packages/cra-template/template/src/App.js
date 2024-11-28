ca2c1a43f2b19e09e0bae2e4410667071393b980
TandimAI <hello@tandim.ai>
2024-11-28 22:54:18 +1300

    commit

---
+++ packages/cra-template/template/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React Frontend Placeholder</h1>
+      <p>This is a placeholder for the React app.</p>
+    </div>
+  );
+}
+
+export default App;
